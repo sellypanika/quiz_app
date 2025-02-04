e082de5cea63d8a0696346025e3afa271a1fa3ca
sellypanika <selly15panika@outlook.com>
2025-02-04 17:39:24 +0200

    Update sessionMiddleware.js

--- app/middlewares/sessionMiddleware.js
+++ app/middlewares/sessionMiddleware.js
@@ -5,8 +5,9 @@ const sessionOptions = {
   storage: new Session.MemoryStorage(),
   cookie: {
     maxAge: 60 * 60 * 1000, // 1 hour session expiry
-     secure: isProduction, 
+     secure: true, 
     httpOnly: true,
+    path: "/",
   },
 };
 
