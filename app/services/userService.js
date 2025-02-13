eea6dc9d21a7cdde1092c16e33b92f882c46f9cf
sellypanika <selly15panika@outlook.com>
2025-02-13 14:36:59 +0200

    Update userService.js

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,6 +1,13 @@
 import { sql } from "../database.js";
 
+const isValidPassword = (password) => {
+    return password.length >= 4;
+};
 const addUser = async (email, password) => {
+    if (!isValidPassword(password)) {
+        throw new Error("Password must be at least 4 characters long.");
+    }
+    
     await sql`INSERT INTO users (email, password) VALUES (${email}, ${password})`;
 };
 
